e4dec9a68f906fa6a0a0438c0baba10417de66e6
Siddhant-dev007 <siddhant2007s@gmail.com>
2026-03-21 19:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.96%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.c
@@ -0,0 +1,14 @@
+int sumOfLeftLeaves(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int sum = 0;
+
+    if (root->left != NULL &&
+        root->left->left == NULL &&
+        root->left->right == NULL) {
+        sum += root->left->val;
+    }
+
+    return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+}
